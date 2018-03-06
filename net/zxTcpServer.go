3fbfb8e93f5a1041df29731a8c4467c572ae4119
z <z@gmail.com>
2018-03-06 23:23:17 +0800

    2018-3-6 23:23:15

---
+++ net/zxTcpServer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/zx9229/zxgo/File"
+)
+
+//go-tcpsock/server.go
+func handleConn(c net.Conn) {
+	defer c.Close()
+	fmt.Println(time.Now(), "RemoteAddr:", c.RemoteAddr().String(), "LocalAddr:", c.LocalAddr().String())
+	buf := make([]byte, 1024)
+	for {
+		lenght, err := c.Read(buf)
+		if err != nil {
+			break
+		}
+		if lenght > 0 {
+			buf[lenght] = 0
+		}
+		fmt.Println(time.Now(), "Rec[", c.RemoteAddr().String(), "] Say :", string(buf[0:lenght]))
+	}
+	fmt.Println(time.Now(), "is_Closeed")
+}
+
+func main() {
+	File.AppendLine("", "", true)
+	l, err := net.Listen("tcp", ":6889")
+	if err != nil {
+		fmt.Println("listen error:", err)
+		return
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			break
+		}
+		// start a new goroutine to handle
+		// the new connection.
+		go handleConn(c)
+	}
+}
